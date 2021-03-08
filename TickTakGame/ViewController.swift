9ce159f6dd22fa53eb5072fc856f96fecffcbf1c
UHS <ioopl.com@gmail.com>
2021-03-08 11:04:15 +0000

    Initial Commit

---
+++ TickTakGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TickTakGame
+//
+//  Created by UHS on 08/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
