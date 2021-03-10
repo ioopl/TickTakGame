b7a2e23105759396f6d78cfccdae0b6886f08970
UHS <ioopl.com@gmail.com>
2021-03-10 22:58:41 +0000

    Added UI Tests

--- TickTakGameUITests/TickTakGameUITests.swift
+++ TickTakGameUITests/TickTakGameUITests.swift
@@ -39,4 +39,107 @@ class TickTakGameUITests: XCTestCase {
             }
         }
     }
+    
+    func setUpMatch(){
+        let app = XCUIApplication()
+        app.textFields["playerOneTextField"].tap()
+        app.textFields["playerOneTextField"].typeText("John")
+        app.textFields["playerTwoTextField"].tap()
+        app.textFields["playerTwoTextField"].typeText("Wick")
+        app.buttons["startButton"].tap()
+    }
+    
+    func testTapButtonOne (){
+        let app = XCUIApplication()
+        let button = app.buttons["buttonOne"]
+        button.tap()
+        XCTAssertEqual(button.label, "X")
+    }
+    
+    func testTapButtonTwo (){
+        let app = XCUIApplication()
+        let button = app.buttons["buttonTwo"]
+        button.tap()
+        XCTAssertEqual(button.label, "X")
+    }
+
+    func testTapButtonThree (){
+        let app = XCUIApplication()
+        let button = app.buttons["buttonThree"]
+        button.tap()
+        XCTAssertEqual(button.label, "X")
+    }
+    
+    func testTapButtonFour (){
+        let app = XCUIApplication()
+        let button = app.buttons["buttonFour"]
+        button.tap()
+        XCTAssertEqual(button.label, "X")
+    }
+    
+    func testTapButtonFive (){
+        let app = XCUIApplication()
+        let button = app.buttons["buttonFive"]
+        button.tap()
+        XCTAssertEqual(button.label, "X")
+    }
+    
+    func testTapButtonSix (){
+        let app = XCUIApplication()
+        let button = app.buttons["buttonSix"]
+        button.tap()
+        XCTAssertEqual(button.label, "X")
+    }
+    
+    func testTapButtonSeven (){
+        let app = XCUIApplication()
+        let button = app.buttons["buttonSeven"]
+        button.tap()
+        XCTAssertEqual(button.label, "X")
+    }
+    
+    func testTapButtonEight (){
+        let app = XCUIApplication()
+        let button = app.buttons["buttonEight"]
+        button.tap()
+        XCTAssertEqual(button.label, "X")
+    }
+    
+    func testTapButtonNine (){
+        let app = XCUIApplication()
+        let button = app.buttons["buttonNine"]
+        button.tap()
+        XCTAssertEqual(button.label, "X")
+    }
+    
+    func testPlayerOneWon() {
+        let app = XCUIApplication()
+        let button = app.buttons["buttonOne"]
+        button.tap()
+        app.buttons["buttonFive"].tap()
+        app.buttons["buttonTwo"].tap()
+        app.buttons["buttonSix"].tap()
+        app.buttons["buttonThree"].tap()
+        
+        let alert = app.alerts["Player One Wins!"]
+        alert.collectionViews.buttons["Ok"].tap()
+        
+        XCTAssertEqual(button.label, "")
+    }
+    
+    func testPlayerTwoWon() {
+        let app = XCUIApplication()
+        let button = app.buttons["buttonOne"]
+        button.tap()
+        app.buttons["buttonFive"].tap()
+        app.buttons["buttonTwo"].tap()
+        app.buttons["buttonSix"].tap()
+        app.buttons["buttonNine"].tap()
+        app.buttons["buttonFour"].tap()
+        
+        let alert = app.alerts["Player Two Wins!"]
+        alert.collectionViews.buttons["Ok"].tap()
+        
+        XCTAssertEqual(button.label, "")
+    }
 }
